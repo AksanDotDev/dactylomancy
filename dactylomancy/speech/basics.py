5d10de223a162c427d616ecc87d9cae447e9379c
Alexi Dewfall <git@aksan.dev>
2022-12-26 17:07:55 +0000

    Context menu options to edit and delete messages

--- dactylomancy/speech/basics.py
+++ dactylomancy/speech/basics.py
@@ -17,6 +17,22 @@ class ReplyModal(discord.ui.Modal, title="Reply"):
         await interaction.response.defer(thinking=False)
 
 
+class EditModal(discord.ui.Modal, title="Edit"):
+    body = discord.ui.TextInput(
+        label="body",
+        style=discord.TextStyle.long
+    )
+
+    def __init__(self, *, message: discord.Message) -> None:
+        super().__init__()
+        self.message = message
+        self.body.default = self.message.content
+
+    async def on_submit(self, interaction: discord.Interaction):
+        await self.message.edit(content=self.body)
+        await interaction.response.defer(thinking=False)
+
+
 async def setup(bot: commands.Bot):
 
     @bot.tree.command()
@@ -37,3 +53,34 @@ async def setup(bot: commands.Bot):
                 message: discord.Message
             ):
         await interaction.response.send_modal(ReplyModal(message=message))
+
+    @bot.tree.context_menu()
+    async def edit(
+                interaction: discord.Interaction,
+                message: discord.Message
+            ):
+        if message.author.id == bot.user.id:
+            await interaction.response.send_modal(EditModal(message=message))
+        else:
+            await interaction.response.send_message(
+                "Only proxied messages can be edited.",
+                ephemeral=True
+            )
+    
+    @bot.tree.context_menu()
+    async def delete(
+                interaction: discord.Interaction,
+                message: discord.Message
+            ):
+        if message.author.id == bot.user.id:
+            await message.delete()
+            await interaction.response.send_message(
+                "Deleted.",
+                ephemeral=True,
+                delete_after=0.0
+            )
+        else:
+            await interaction.response.send_message(
+                "Only proxied messages can be deleted.",
+                ephemeral=True
+            )
