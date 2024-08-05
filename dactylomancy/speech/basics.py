ac46f8a2a1fba5aca60d66f0e103022890761ab4
Alexi Dewfall <git@aksan.dev>
2024-08-05 17:31:07 +0100

    "Upload" overhauled and improved, multiple files, better UX

--- dactylomancy/speech/basics.py
+++ dactylomancy/speech/basics.py
@@ -26,6 +26,7 @@ async def setup(bot: commands.Bot):
     if "interface" not in bot.zeroth_ring:
         config_table = tomlkit.table()
         config_table["timeout"] = 0.0
+        config_table["silent_success"] = True
         bot.zeroth_ring["interface"] = config_table
 
     @bot.tree.command()
@@ -48,6 +49,7 @@ async def setup(bot: commands.Bot):
                 await interaction.response.send_message(
                     "Replied.",
                     ephemeral=True,
+                    silent=bot.zeroth_ring["interface"]["silent_success"],
                     delete_after=bot.zeroth_ring["interface"]["timeout"]
                 )
             except ValueError:
@@ -68,6 +70,7 @@ async def setup(bot: commands.Bot):
             await interaction.response.send_message(
                 "Sent.",
                 ephemeral=True,
+                silent=bot.zeroth_ring["interface"]["silent_success"],
                 delete_after=bot.zeroth_ring["interface"]["timeout"]
             )
 
@@ -91,6 +94,7 @@ async def setup(bot: commands.Bot):
                 await interaction.response.send_message(
                     "Thread created.",
                     ephemeral=True,
+                    silent=bot.zeroth_ring["interface"]["silent_success"],
                     delete_after=bot.zeroth_ring["interface"]["timeout"]
                 )
             except ValueError:
@@ -114,6 +118,7 @@ async def setup(bot: commands.Bot):
             await interaction.response.send_message(
                 "Thread created.",
                 ephemeral=True,
+                silent=bot.zeroth_ring["interface"]["silent_success"],
                 delete_after=bot.zeroth_ring["interface"]["timeout"]
             )
 
@@ -122,18 +127,100 @@ async def setup(bot: commands.Bot):
         interaction: discord.Interaction,
         attachment: discord.Attachment,
         spoiler: Optional[bool] = False,
-        caption: Optional[str] = ""
+        caption: Optional[str] = "",
+        reply: Optional[str] = None,
+        mention: Optional[bool] = False,
+        silent: Optional[bool] = False,
+        ancillary_i: Optional[discord.Attachment] = None,
+        ancillary_ii: Optional[discord.Attachment] = None,
+        ancillary_iii: Optional[discord.Attachment] = None,
+        ancillary_iv: Optional[discord.Attachment] = None,
+        ancillary_v: Optional[discord.Attachment] = None,
+        ancillary_vi: Optional[discord.Attachment] = None,
+        ancillary_vii: Optional[discord.Attachment] = None,
+        ancillary_viii: Optional[discord.Attachment] = None,
+        ancillary_ix: Optional[discord.Attachment] = None,
     ):
-        att_file = await attachment.to_file(spoiler=bool(spoiler))
-        await interaction.channel.send(
-            emojify(caption),
-            file=att_file
-        )
-        await interaction.response.send_message(
-            "Uploaded.",
+        await interaction.response.defer(
             ephemeral=True,
-            delete_after=bot.zeroth_ring["interface"]["timeout"]
+            thinking=True
         )
+        attachments = [
+            attachment, ancillary_i, ancillary_ii, ancillary_iii,
+            ancillary_iv, ancillary_v, ancillary_vi, ancillary_vii,
+            ancillary_viii, ancillary_ix
+        ]
+
+        att_files = []
+        skipped = []
+
+        for file in attachments:
+            if file:
+                if file.size > interaction.guild.filesize_limit:
+                    skipped.append(file.filename)
+                else:
+                    att_files.append(
+                        await file.to_file(spoiler=spoiler)
+                    )
+
+        if not att_files:
+            await interaction.followup.send(
+                content="No file below server file size limit attached, message not sent."
+            )
+
+        if reply:
+            try:
+                msg_id = get_message_snowflake(reply)
+                msg = interaction.channel.get_partial_message(msg_id)
+
+                await msg.reply(
+                    emojify(caption),
+                    mention_author=mention,
+                    silent=silent,
+                    files=att_files
+                )
+                if skipped:
+                    await interaction.followup.send(
+                        content="Uploaded as a reply, while skipping: "
+                        + f"{', '.join(skipped)}, due to file sizes.",
+                        ephemeral=True,
+                        silent=bot.zeroth_ring["interface"]["silent_success"]
+                    )
+                else:
+                    await interaction.followup.send(
+                        content="Uploaded as a reply.",
+                        ephemeral=True,
+                        silent=bot.zeroth_ring["interface"]["silent_success"]
+                    )
+            except ValueError:
+                await interaction.followup.send(
+                    content="Error in reply pararmeter, could not parse.",
+                    ephemeral=True
+                )
+            except TypeError:
+                await interaction.followup.send(
+                    content="Error in replying, could not find message.",
+                    ephemeral=True
+                )
+        else:
+            await interaction.channel.send(
+                emojify(caption),
+                silent=silent,
+                files=att_files
+            )
+            if skipped:
+                await interaction.followup.send(
+                    content="Uploaded, while skipping: "
+                    + f"{', '.join(skipped)}, due to file sizes.",
+                    ephemeral=True,
+                    silent=bot.zeroth_ring["interface"]["silent_success"]
+                )
+            else:
+                await interaction.followup.send(
+                    content="Uploaded.",
+                    ephemeral=True,
+                    silent=bot.zeroth_ring["interface"]["silent_success"]
+                )
 
     @bot.tree.context_menu()
     async def edit(
@@ -158,6 +245,7 @@ async def setup(bot: commands.Bot):
             await interaction.response.send_message(
                 "Deleted.",
                 ephemeral=True,
+                silent=bot.zeroth_ring["interface"]["silent_success"],
                 delete_after=bot.zeroth_ring["interface"]["timeout"]
             )
         else:
