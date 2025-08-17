5ac4b65b6579f09aa536bb62679234c40f6dd22f
Alexi Dewfall <git@aksan.dev>
2025-08-17 16:23:48 +0100

    Upversion

--- dactylomancy/__init__.py
+++ dactylomancy/__init__.py
@@ -3,6 +3,7 @@ __author__ = 'AksanDotDev'
 __url__ = 'https://dactylomancy.aksan.dev/'
 __license__ = 'MIT'
 __copyright__ = 'Copyright 2022-present AksanDotDev'
+__version__ = '0.2.12'
 __dactylomancy__ = [
     'text.commands',
     'text.context_menus',
